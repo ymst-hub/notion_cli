6785bdd1c53ebd20adaa6b142dcd006bfc725a38
ymst-hub <64212002+ymst-hub@users.noreply.github.com>
2022-10-27 21:32:10 +0900

    ファイル作成と変更ができるようになった

--- src/file_local.rs
+++ src/file_local.rs
@@ -4,31 +4,31 @@ use std::{
     io::{self, Write},
 };
 
+const FILENAME: &str = "./env.local";
+
 pub fn create_local_env() {
-    let filename = "./env.local";
     let secret_key = input_std("シークレットキーを入力してください");
     let database_name = input_std("データベースの名前を入力してください");
-    let mut file = File::create(filename.trim()).expect("ファイル作成エラー");
+    let mut file = File::create(FILENAME.trim()).expect("ファイル作成エラー");
     let write_buf = format!("{}\n{}", secret_key, database_name);
     file.write_all(write_buf.as_bytes())
         .expect("ファイル書き込みエラー");
 }
 
-fn input_std(print_sentence: &str) -> String {
+pub fn read_local_env() -> Result<Vec<String>,io::Error> {
+    let sentence = fs::read_to_string(FILENAME)?;
+    let buf = sentence.split('\n').fold(Vec::new(), |mut s, i| {
+        s.push(i.to_string());
+        s
+    });
+    Ok(buf)
+}
+
+pub fn input_std(print_sentence: &str) -> String {
     println!("{}", print_sentence);
     let mut sentence = String::new();
     io::stdin()
         .read_line(&mut sentence)
         .expect("文字入力エラー");
     sentence.trim().to_string()
-}
-
-pub fn read_local_env() -> Vec<String> {
-    let filename = "./env.local";
-    let sentence = fs::read_to_string(filename).expect("ファイル読み込みエラー");
-    let buf = sentence.split('\n').fold(Vec::new(), |mut s, i| {
-        s.push(i.to_string());
-        s
-    });
-    buf
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 mod file_local;
 fn main() {
-    file_local::create_local_env();
-    for a in file_local::read_local_env() {
-        println!("{}", a);
+    //ファイルを開きキー設定を確認する。
+    let keys: Vec<String> = match file_local::read_local_env() {
+        Ok(val) => val,
+        Err(_) => {
+            println!("Notionのキー設定がされていません。");
+            file_local::create_local_env();
+            std::process::exit(1);
+        },
+    };
+
+    //コマンドを取得し、matchで判定する
+    loop {
+        let command_print = "コマンドを入力してください\n1 : ボードを全件取得\n2 : タスクのフラグを変更する\n3 : キーの再設定\nその他 : 終了する";
+        let command:i64 = match file_local::input_std(command_print).parse(){
+            Ok(val) => val,
+            Err(_) => std::process::exit(2),
+        };
+        match command {
+            1 => todo!(),//ボードの全件取得
+            2 => todo!(),//タスクの移動
+            3 => file_local::create_local_env(),//ファイルの再設定
+            _ => std::process::exit(3),//終了
+        }
     }
+
 }
