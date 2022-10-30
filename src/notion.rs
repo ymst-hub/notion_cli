1aa2a8b9f6c7a0c8c517b0c830c841ca382c3f9d
ymst-hub <64212002+ymst-hub@users.noreply.github.com>
2022-10-31 00:15:06 +0900

    コミット

--- src/notion.rs
+++ src/notion.rs
@@ -1,13 +1,23 @@
 use reqwest::Client;
 
+const NOTION_BASE_URL: &str = "https://api.notion.com/v1/";
+const NOTION_VER: &str = "2022-06-28";
+
+//全件取得
 pub async fn fetch_notion_all_table(secret_key: String, database_name: String) {
     let client = Client::new();
-    let url = "https://zipcloud.ibsnet.co.jp/api/search";
+    let url = format!("{}{}{}", NOTION_BASE_URL, "databases/", database_name,);
     let response = client
         .get(url)
-        .query(&[("zipcode", "1000002")])
+        //ヘッダー部
+        .header(
+            "Authorization",
+            &format!("{} {}", "Bearer", &secret_key) as &str,
+        )
+        .header("Notion-Version", NOTION_VER)
         .send()
         .await
-        .unwrap();
-    println!("{}", response.text().await.unwrap())
+        .expect("api疎通エラー");
+    let response_json = response.text().await.unwrap();
+    println!("{}", response_json);
 }
