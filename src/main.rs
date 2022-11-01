f6ff349233c60f3a83193a4d34bbd36d2cb9360a
ymst-hub <64212002+ymst-hub@users.noreply.github.com>
2022-11-01 21:44:17 +0900

    コミット

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,14 @@ async fn main() -> std::io::Result<()> {
     loop {
         let secret_key = keys_buf.get(0).unwrap().parse().unwrap();
         let database_name = keys_buf.get(1).unwrap().parse().unwrap();
-        let command_print = "コマンドを入力してください\n1 : ボードを全件取得\n2 : タスクのフラグを変更する\n3 : キーの再設定\nその他 : 終了する";
+        let command_print = "コマンドを入力してください\n1 : データベース情報取得\n2 : 指定したタグの情報を取得\n3 : キーの再設定\nその他 : 終了する";
         let command: i64 = match file_local::input_std(command_print).parse() {
             Ok(val) => val,
             Err(_) => std::process::exit(2),
         };
         match command {
-            1 => notion::fetch_notion_all_table(secret_key, database_name).await, //ボードの全件取得
-            2 => todo!(),                                                         //タスクの移動
+            1 => notion::auth_notion(secret_key, database_name).await,
+            2 => notion::fetch_notion_all_table(secret_key, database_name).await,
             3 => file_local::create_local_env(),                                  //ファイルの再設定
             _ => std::process::exit(3),                                           //終了
         };
--- src/notion.rs
+++ src/notion.rs
@@ -4,11 +4,38 @@ use serde_json::json;
 const NOTION_BASE_URL: &str = "https://api.notion.com/v1/";
 const NOTION_VER: &str = "2022-06-28";
 
+pub async fn auth_notion(secret_key: String, database_name: String){
+    let client = Client::new();
+    let url = format!(
+        "{}{}{}",
+        NOTION_BASE_URL, "databases/", database_name,
+    );
+    let response = client
+        .get(url)
+        //ヘッダー部
+        .header(
+            "Authorization",
+            &format!("{} {}", "Bearer", &secret_key) as &str,
+        )
+        .header("Notion-Version", NOTION_VER)
+        .send()
+        .await
+        .expect("api疎通エラー");
+    let response_json = response.text().await.unwrap();
+    println!("{}", response_json);
+    
+}
+
 //全件取得
 pub async fn fetch_notion_all_table(secret_key: String, database_name: String) {
-    let post_body = json!({
+    //検索内容
+    let post_body = json! ({
             "filter":{
-    }});
+                "property": "tag",
+            }
+        }
+    );
+    
     let client = Client::new();
     let url = format!(
         "{}{}{}{}",
@@ -23,6 +50,7 @@ pub async fn fetch_notion_all_table(secret_key: String, database_name: String) {
         )
         .header("Notion-Version", NOTION_VER)
         .header("Content-Type", "application/json")
+
         .json(&post_body)
         .send()
         .await
